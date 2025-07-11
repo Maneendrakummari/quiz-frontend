bc65ede3559f1086661edbd3c18b9fb2a951a01a
Maneendrakummari <147535164+Maneendrakummari@users.noreply.github.com>
2025-07-11 19:10:33 +0530

    commit

--- src/components/QuizResults.js
+++ src/components/QuizResults.js
@@ -4,7 +4,7 @@ const QuizResults = () => {
   const [results, setResults] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8080/api/questions/results")
+fetch("https://quizapplication-5.onrender.com/api/questions/results")
       .then((res) => res.json())
       .then((data) => setResults(data))
       .catch((err) => console.error("Error loading quiz results:", err));
