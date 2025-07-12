4a96622b06924a4995c997cdfb232200cd176e83
Maneendrakummari <147535164+Maneendrakummari@users.noreply.github.com>
2025-07-12 12:43:32 +0530

    commit

--- src/admin/AdminLogin.js
+++ src/admin/AdminLogin.js
@@ -13,7 +13,7 @@ const AdminLogin = () => {
     setError("");
 
     axios
-      .post("https://quizapplication-5.onrender.com/api/admin/login", { username, password })
+      .post("https://quizapplication-6.onrender.com/api/admin/login", { username, password })
       .then((res) => {
         localStorage.setItem("adminLoggedIn", "true");
         localStorage.setItem("adminAuth", res.data.token); // optional token
